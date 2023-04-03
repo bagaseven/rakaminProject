f34cbc35fb4f283b133eb0e39720a6f820c2e0c9
bagasandaryanto <bagas.andaryanto@beautyfulminds.com.sg>
2023-04-03 21:52:46 +0700

    Login Scenario For Rakamin

---
+++ cypress/e2e/rakaminProject.cy.js
@@ -0,0 +1,77 @@
+context('RakaminProject', () => {
+  beforeEach(() => {
+    cy.visit('https://web-staging.rakamin.com/')
+  })
+
+  describe('go to rakamin staging web and login', () => {
+    it('verify that logo is visible', () => {
+      cy.get('a[data-cy=rakamin-logo]').should('be.visible');
+    })
+
+    it('login with existing account', () => {
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('button[data-cy=login-page-button').should('be.visible');
+      cy.contains('Login').click({force: true});
+      cy.get('input[name=email]').type("ayamgoreng@mailinator.com");
+      cy.get('input[name=password]').type("ayamgorengenaksekali");
+      cy.get('button[data-cy=login-submit-button').click();
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('span').should('contain', 'Ayam');
+    })
+
+    it('login with false password', () => {
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('button[data-cy=login-page-button').should('be.visible');
+      cy.contains('Login').click({force: true});
+      cy.get('input[name=email]').type("ayamgoreng@mailinator.com");
+      cy.get('input[name=password]').type("ayamgoreng");
+      cy.get('button[data-cy=login-submit-button').click();
+      cy.get('div').should('contain', 'Kata sandi anda salah');
+    })
+    
+    it('login with unregistered email', () => {
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('button[data-cy=login-page-button').should('be.visible');
+      cy.contains('Login').click({force: true});
+      cy.get('input[name=email]').type("ayamgorengsatu@mailinator.com");
+      cy.get('input[name=password]').type("ayamgoreng");
+      cy.get('button[data-cy=login-submit-button').click();
+      cy.get('div').should('contain', 'Email ini belum terdaftar sebagai akun di Rakamin Academy.');
+    })
+
+    it('login with wrong email format', () => {
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('button[data-cy=login-page-button').should('be.visible');
+      cy.contains('Login').click({force: true});
+      cy.get('input[name=email]').type("ayamgoreng.mailinator.com");
+      cy.get('input[name=password]').type("ayamgoreng");
+      cy.get('button[data-cy=login-submit-button').click();
+      cy.get('div').should('contain', 'Alamat email tidak valid');
+    })
+
+    it('login with empty email', () => {
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+      cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+      cy.get('button[data-cy=login-page-button').should('be.visible');
+      cy.contains('Login').click({force: true});
+      cy.get('input[name=password]').type("ayamgoreng");
+      cy.get('button[data-cy=login-submit-button').click();
+      cy.get('div').should('contain', 'Alamat email tidak boleh kosong');
+    })
+
+    it('login with empty password', () => {
+        cy.get("img[src='/assets/hamburger.d750d25a.svg']").should('be.visible');
+        cy.get("img[src='/assets/hamburger.d750d25a.svg']").click();
+        cy.get('button[data-cy=login-page-button').should('be.visible');
+        cy.contains('Login').click({force: true});
+        cy.get('input[name=email]').type("ayamgoreng@mailinator.com");
+        cy.get('button[data-cy=login-submit-button').click();
+        cy.get('div').should('contain', 'Kata sandi tidak boleh kosong');
+      })
+  })
+
+})
